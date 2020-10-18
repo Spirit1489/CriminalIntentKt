c4713b288d62de292a541b6cfead58750720543b
Spirit1489 <Spirit1489@gmail.com>
2020-10-18 10:48:51 +0300

    Room1

---
+++ app/src/main/java/ru/spiritblog/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,19 @@
+package ru.spiritblog.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import ru.spiritblog.criminalintent.Crime
+import java.util.*
+
+@Dao
+interface CrimeDao {
+
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+
+
+}
---
+++ app/src/main/java/ru/spiritblog/criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,16 @@
+package ru.spiritblog.criminalintent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ru.spiritblog.criminalintent.Crime
+
+@Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
+
+abstract class CrimeDatabase : RoomDatabase() {
+
+    abstract fun crimeDao(): CrimeDao
+
+
+}
