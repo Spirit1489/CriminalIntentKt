048367d7001437f8367f893d406cbce358982bf3
Spirit1489 <Spirit1489@gmail.com>
2020-10-22 16:05:08 +0300

    Callbacks, second Activity

--- app/src/main/java/ru/spiritblog/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/ru/spiritblog/criminalintent/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package ru.spiritblog.criminalintent
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,18 +10,32 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_date.*
+import java.util.*
 
 private const val TAG = "CrimeListFragment"
 
 
 class CrimeListFragment : Fragment() {
 
+    /**
+     * Required interface for hosting activities
+     */
+
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+
+
+    private var callbacks: Callbacks? = null
+
+
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter: CrimeAdapter? = null
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
@@ -29,13 +44,10 @@ class CrimeListFragment : Fragment() {
 
     }
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,16 +60,39 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView = view.findViewById(R.id.recyclerView) as RecyclerView
 
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
+        crimeRecyclerView.adapter = adapter
 
         return view
 
     }
 
 
-    private fun updateUI() {
-        val crimes = crimeListViewModel.crimes
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+
+
+            }
+
+
+        )
+
+
+    }
+
+
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+
+    private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
@@ -91,8 +126,7 @@ class CrimeListFragment : Fragment() {
 
 
         override fun onClick(v: View?) {
-            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT)
-                .show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
 
--- app/src/main/java/ru/spiritblog/criminalintent/database/CrimeDao.kt
+++ app/src/main/java/ru/spiritblog/criminalintent/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package ru.spiritblog.criminalintent.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import ru.spiritblog.criminalintent.Crime
@@ -9,11 +10,11 @@ import java.util.*
 interface CrimeDao {
 
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 
 
 }
